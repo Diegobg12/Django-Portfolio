f94a3bc252ff6dc0bdc57b3faa5b15a09af38943
Diego <diegobustos1229@gmail.com>
2020-03-16 16:13:22 -0700

    add front end

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=20)
+    pub_date =  models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# Create A blog Models
+
+# Create a migration
+
+# Migrate
+
+# Add the admin
