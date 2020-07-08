91a8ba1016ce8ef4ea632745236004ac892bd7b4
Diego <diegobustos1229@gmail.com>
2020-07-07 19:45:12 -0700

    all blogs page style

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,8 @@ from django.shortcuts import render
 
 def allblogs(request):
     blogs = Blog.objects
-    return render(request, 'allblogs.html', {'blogs': blogs})
+    last = Blog.objects.latest('pub_date')
+    return render(request, 'allblogs.html', {'blogs': blogs, 'last':last})
 
 def detail(request, blog_id):
     detailblog = get_object_or_404(Blog, pk=blog_id)
